0502f0dda211741c3de82dd7acdf58a09cd3ca1d
Ian Zamojc <izamojc@zamtools.com>
2021-05-18 14:39:11 -0400

    extra test

--- components/__tests__/Poster.spec.tsx
+++ components/__tests__/Poster.spec.tsx
@@ -4,19 +4,27 @@ import { shallow } from 'enzyme'
 import Poster from '../Poster'
 
 describe('Poster component', () => {
-  it('should display show details', () => {
-    const show: Show = {
-      id: '1a',
-      title: 'show title',
-      episodes: 5,
-      productImageUrl: 'whatever.jpg',
-    }
+  const show: Show = {
+    id: '1a',
+    title: 'show title',
+    episodes: 5,
+    productImageUrl: 'whatever.jpg',
+  }
 
+  it('should display show details', () => {
     const component = shallow(
       <Poster show={show} />
     )
 
     expect(component.find('[role="episodes"]').text()).toEqual('5 Episodes')
     expect(component.find('[role="title"]').text()).toEqual('show title')
+  })
+
+  it('should pass classes', () => {
+    const component = shallow(
+      <Poster show={show} className="ding" />
+    )
+
+    expect(component.hasClass('ding')).toEqual(true)
   })
 })
