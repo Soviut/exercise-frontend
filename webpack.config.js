37dd4fe8e75591ede44d0ac8a5c6a0f22236b083
Ian Zamojc <izamojc@zamtools.com>
2021-05-18 00:21:13 -0400

    tailwind

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,13 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
+
   output: {
     publicPath: '/',
   },
+
   entry: './index.tsx',
+
   module: {
     rules: [
       {
@@ -24,23 +27,31 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
     ],
   },
+
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: 'index.html',
     }),
     new webpack.HotModuleReplacementPlugin(),
   ],
+
   devtool: 'inline-source-map',
+
   devServer: {
     contentBase: path.join(__dirname, 'build'),
     historyApiFallback: true,
     port: 4000,
-    open: true,
     hot: true
   },
 }
