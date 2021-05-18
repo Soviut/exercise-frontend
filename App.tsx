ace0cc398f86e8cc686d3f22de4a38982d16732f
Ian Zamojc <izamojc@zamtools.com>
2021-05-18 01:56:15 -0400

    react router

--- App.tsx
+++ App.tsx
@@ -1,21 +1,35 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
 
-import HomePage from './pages'
+import GalleryPage from './pages/gallery'
 
 export default () => {
   return (
-    <div>
-      <header>
-        <nav>
-          <a href="/">
-            Vice
-          </a>
-        </nav>
-      </header>
+    <Router>
+      <div>
+        <header>
+          <nav>
+            {/* TODO: put branding and nav here */}
+          </nav>
+        </header>
 
-      <main className="container mx-auto">
-        <HomePage />
-      </main>
-    </div>
+        <main className="container mx-auto">
+          <Switch>
+            <Route exact path="/">
+              <Redirect to="/gallery" />
+            </Route>
+
+            <Route path="/gallery">
+              <GalleryPage />
+            </Route>
+          </Switch>
+        </main>
+      </div>
+    </Router>
   )
 }
